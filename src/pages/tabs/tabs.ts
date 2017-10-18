66a095b7b434079dbc85951c51f150902d731058
andyrew <bing.liu@61topbaby.com>
2017-10-18 09:31:56 +0800

    弹出层继续扫码按钮去掉右边框

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Help } from '../help/help';
+import { GiftInfo } from '../gift-info/gift-info';
 import { Login } from '../login/login';
 import { ListPage } from '../list/list';
 @Component({
   templateUrl: 'tabs.html'
 }) 
 export class TabsPage {
-  tab1Root = Help;
+  tab1Root = GiftInfo;
   tab2Root = Login;
   tab3Root = ListPage;
   constructor() {
