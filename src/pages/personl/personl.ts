99ec427d5ffc333f10efeb774c8a6d6c6382da04
andyrew <bing.liu@61topbaby.com>
2017-10-23 15:14:57 +0800

    我的页面-金额显示或隐藏

--- src/pages/personl/personl.ts
+++ src/pages/personl/personl.ts
@@ -5,8 +5,22 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'personl.html'
 })
 export class Personl {
-  constructor(public navCtrl: NavController) {
-  }
+	total: number = 69922.36;
+	gotMoney: number = 2689.63;
+	unGetMoney: number = 18889.62;
+	isStar: boolean = false;
+	showImg: string = 'hide.png';
+	showText: string = '隐藏';
+	constructor(public navCtrl: NavController) {
+	}
+	showMoney() {
+		this.isStar = !this.isStar;
+		this.showText = !this.isStar ? '隐藏' : '显示';
+		this.showImg = !this.isStar ? 'hide.png' : 'show.png';
+		this.total = !this.isStar ?  69922.36 : '*****';
+		this.gotMoney = !this.isStar ?  2689.63 : '*****';
+		this.unGetMoney = !this.isStar ?  18889.62 : '*****';
+	}
   
   
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { CreatOrder } from '../creat-order/creat-order';
-import { Home } from '../home/home';
+import { Personl } from '../personl/personl';
 import { Login } from '../login/login';
 @Component({
   templateUrl: 'tabs.html'
 }) 
 export class TabsPage {
-  tab1Root = Home;
+  tab1Root = Personl;
   tab2Root = Login;
   tab3Root = Login;
   constructor() {
