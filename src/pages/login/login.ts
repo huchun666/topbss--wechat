fb5dbbac5c635a9bf4366c387e3746074c3d3834
andyrew <bing.liu@61topbaby.com>
2017-11-02 15:18:10 +0800

    登录页模拟调整

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -18,12 +18,9 @@ export class Login {
   ) {
   }
   login() {
-    if (this.userName != "13761489650" || this.pwd != "123456lb") {
-      this.isNameAndPwd = true;
+    if (this.userName == "13761489650" || this.pwd == "123456lb") {
       let appNav = this.app.getRootNav();
       appNav.setRoot(TabsPage);
-    } else {
-      this.isNameAndPwd = true;
     }
   
     // 登陆实际是post或者put，暂时先使用get模拟
