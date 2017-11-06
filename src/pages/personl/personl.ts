b659666672a9d10ac133a46a7b88c8c5b0b6d962
andyrew <bing.liu@61topbaby.com>
2017-11-06 15:24:08 +0800

    我的-页面跳转方法封装和页面跳转添加

--- src/pages/personl/personl.ts
+++ src/pages/personl/personl.ts
@@ -4,7 +4,10 @@ import { Withdraw } from '../withdraw/withdraw';
 import { Login } from '../login/login';
 import { MyCode } from '../mycode/mycode';
 import { DetailTabs } from '../detail-tabs/detail-tabs';
-import { UnauditTabs } from '../unaudit-tabs/unaudit-tabs';
+import { AwardTabs } from '../award-tabs/award-tabs';
+import { WithdrawRecord } from '../withdraw-record/withdraw-record';
+import { Help } from '../help/help';
+import { BindAccount } from '../account/bind-account/bind-account';
 @Component({
   selector: 'personl',
   templateUrl: 'personl.html'
@@ -16,6 +19,7 @@ export class Personl {
   isStar: boolean = false;
   showImg: string = 'hide.png';
   showText: string = '隐藏';
+  pageList: any = null;
   constructor(
     public nav: Nav,
     public navCtrl: NavController,
@@ -23,7 +27,17 @@ export class Personl {
     public viewCtrl: ViewController,
     private app: App
   ) {
+    this.pageList = {
+      "withdraw": Withdraw,
+      "myCode": MyCode,
+      "detailTabs": DetailTabs,
+      "awardTabs": AwardTabs,
+      "withdrawRecord": WithdrawRecord,
+      "bindAccount": BindAccount,
+      "help": Help
+    }
   }
+  // 显示和隐藏金额
   showMoney() {
     this.isStar = !this.isStar;
     this.showText = !this.isStar ? '隐藏' : '显示';
@@ -32,20 +46,14 @@ export class Personl {
     this.gotMoney = !this.isStar ?  '2689.63' : '*****';
     this.unGetMoney = !this.isStar ?  '18889.62' : '*****';
   }
-  withdraw() {
-    let withdrawModal = this.modalCtrl.create(Withdraw);
-    withdrawModal.present();
-  }
+  // 退出登录
   logOut() {
     let appNav = this.app.getRootNav();
     appNav.setRoot(Login);
   }
-  goMyCode() {
-    let myCodeModal = this.modalCtrl.create(MyCode);
-    myCodeModal.present();
-  }
-  toDetailTabs() {
-    let detailTabs = this.modalCtrl.create(DetailTabs);
-    detailTabs.present();
+  // 跳转页面
+  redirectPage(page) {
+    let pageModal = this.modalCtrl.create(page);
+    pageModal.present();
   }
 }
