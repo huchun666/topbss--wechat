2796ca137447334b328f6b8991b6e783b0380ce9
andyrew <bing.liu@61topbaby.com>
2017-11-08 21:16:10 +0800

    提现-功能

--- src/pages/withdraw/withdraw.ts
+++ src/pages/withdraw/withdraw.ts
@@ -1,10 +1,36 @@
 import { Component} from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { AppService, AppConfig } from '../../app/app.service';
 @Component({
   selector: 'withdraw',
   templateUrl: 'withdraw.html'
 })
 export class Withdraw {
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+  amout: number;
+  allAmout: number = 123.00;
+  isAllow: boolean = true;
+  constructor(
+    public navCtrl: NavController, 
+    public alertCtrl: AlertController,
+    public appService: AppService
+  ) {
+  }
+  withdraw() {
+    console.log(2222);
+    // 防重复提交
+    if (!this.isAllow) {
+      return;
+    }
+    this.isAllow = false;
+    // let url = `${AppConfig.API}`;
+    // let body = {
+    //   "amout": this.amout
+    // };
+    // this.appService.httpPost(url,body).then(data => {
+    //   this.isAllow = true;
+    // }).catch(error => {
+    //   console.log(error);
+    //   this.isAllow = true
+    // });
   }
 }
