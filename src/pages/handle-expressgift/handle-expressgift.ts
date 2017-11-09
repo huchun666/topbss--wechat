b9ab62dd8279d4c529e168006e9f4ccd077276f4
胡春 <haru.hu@61topbaby.com>
2017-11-09 11:32:55 +0800

    赠品服务对接接口数据渲染模拟

--- src/pages/handle-expressgift/handle-expressgift.ts
+++ src/pages/handle-expressgift/handle-expressgift.ts
@@ -1,74 +1,259 @@
 import { Component} from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { AppService, AppConfig } from '../../app/app.service';
 @Component({
   selector: 'handle-expressgift',
   templateUrl: 'handle-expressgift.html'
 })
 export class HandleExpressgift {
   handleExpressGiftArray: any;
-	constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
-    // 获取已兑换快递赠品列表
+  start: number;
+  showNoMoreGift: Boolean = true;
+  noData: Boolean;
+  up: Boolean;//上拉刷新和第一次进入页面时
+  down: Boolean;//下拉刷新和返回上一级页面时
+	constructor(
+		public navCtrl: NavController, 
+		public alertCtrl: AlertController,
+		public appConFig: AppConfig, 
+		public appService: AppService,
+	) {
+	// 获取已兑换快递赠品列表
+	// this.down = false;
+	// this.up = true;
     // getHandleExpressGiftList()
     this.handleExpressGiftArray = [
 			{
-				id: 1,
-				name: "爱法呗赠品小马甲",
-				memberPhone: "12131415161",
-				subscribePhone: "222222222",
-        getTime: "2017.09.27 23:59",
-        subscribeTime: "2017.09.28 23:59",
-        shoppingGuideName: "周三",
-				imgUrl: "../assets/image/productimg.png",
-				subscribeState: "2",//1: 未兑换，2: 已兑换
-				patriarchName: "张家长",
-				patriarchPhone: "12313231545",
-				babyName: "张宝宝",
-				babyHeight: "120",
-				babyIDNumber: "32088888888888",
-				remarkInfo: "圆通物流  12341234123"
+				"memberGiftAccountSeq": 260,
+				"giftSeq": 11,
+				"giftCode": "COUPON00020154",
+				"giftName": "爱法呗赠品小马甲1",
+				"giftType": "2",
+				"imageName": "../assets/image/productimg.png",
+				"giftRemark": "圆通物流  12341234123",
+				"brandshopSeq": 133,
+				"brandshopName": "1001夜杭州旗舰店",
+				"startDate": 1492706727000,
+				"endDate": 1493916327000,
+				"status": "4",
+				"receiveDate": 1492706727000,
+				"useDate": 1492713204000,
+				"memberSeq": 435,
+				"memberPhone": "18366155533",
+				"reservePhone": null,
+				"reserveShopTime": null,
+				"expressCompany": null,
+				"expressNo": null,
+				"deliveryTime": null,
+				"brandshopUserSeq": null,
+				"brandshopUserName": "张三",
+				"attrValueList": [
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 1,
+						"label": "家长姓名",
+						"name": "receiverName",
+						"value": "家长1"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "家长手机号",
+						"name": "detailAddress",
+						"value": "1388888888"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "宝宝姓名",
+						"name": "detailAddress",
+						"value": "宝宝1"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "宝宝身高",
+						"name": "detailAddress",
+						"value": "120"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "宝宝证件号",
+						"name": "detailAddress",
+						"value": "32099999999999999"
+					},
+				], 
 			},
 			{
-				id: 1,
-				name: "爱法呗赠品小马甲",
-				memberPhone: "12131415161",
-				subscribePhone: "222222222",
-        getTime: "2017.09.27 23:59",
-        subscribeTime: "2017.09.28 23:59",
-        shoppingGuideName: "周四",
-				imgUrl: "../assets/image/productimg.png",
-				subscribeState: "2",//1: 未兑换，2: 已兑换
-				patriarchName: "张家长",
-				patriarchPhone: "12313231545",
-				babyName: "张宝宝",
-				babyHeight: "120",
-				babyIDNumber: "32088888888888",
-				remarkInfo: "圆通物流  12341234123"
+				"memberGiftAccountSeq": 260,
+				"giftSeq": 11,
+				"giftCode": "COUPON00020154",
+				"giftName": "爱法呗赠品小马甲2",
+				"giftType": "2",
+				"imageName": "../assets/image/productimg.png",
+				"giftRemark": "圆通物流  12341234123",
+				"brandshopSeq": 133,
+				"brandshopName": "1001夜杭州旗舰店",
+				"startDate": 1492706727000,
+				"endDate": 1493916327000,
+				"status": "4",
+				"receiveDate": 1492706727000,
+				"useDate": 1492713204000,
+				"memberSeq": 435,
+				"memberPhone": "18366155533",
+				"reservePhone": null,
+				"reserveShopTime": null,
+				"expressCompany": null,
+				"expressNo": null,
+				"deliveryTime": null,
+				"brandshopUserSeq": null,
+				"brandshopUserName": "张四",
+				"attrValueList": [
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 1,
+						"label": "家长姓名",
+						"name": "receiverName",
+						"value": "家长1"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "家长手机号",
+						"name": "detailAddress",
+						"value": "1388888888"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "宝宝姓名",
+						"name": "detailAddress",
+						"value": "宝宝1"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "宝宝身高",
+						"name": "detailAddress",
+						"value": "120"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "宝宝证件号",
+						"name": "detailAddress",
+						"value": "32099999999999999"
+					},
+				], 
 			},
 			{
-				id: 1,
-				name: "爱法呗赠品小马甲",
-				memberPhone: "12131415161",
-				subscribePhone: "222222222",
-        getTime: "2017.09.27 23:59",
-        subscribeTime: "2017.09.28 23:59",
-        shoppingGuideName: "周五",
-				imgUrl: "../assets/image/productimg.png",
-				subscribeState: "2",//1: 未兑换，2: 已兑换
-				patriarchName: "张家长",
-				patriarchPhone: "12313231545",
-				babyName: "张宝宝",
-				babyHeight: "120",
-				babyIDNumber: "32088888888888",
-				remarkInfo: "圆通物流  12341234123"
-			}
+				"memberGiftAccountSeq": 260,
+				"giftSeq": 11,
+				"giftCode": "COUPON00020154",
+				"giftName": "爱法呗赠品小马甲2",
+				"giftType": "2",
+				"imageName": "../assets/image/productimg.png",
+				"giftRemark": "圆通物流  12341234123",
+				"brandshopSeq": 133,
+				"brandshopName": "1001夜杭州旗舰店",
+				"startDate": 1492706727000,
+				"endDate": 1493916327000,
+				"status": "4",
+				"receiveDate": 1492706727000,
+				"useDate": 1492713204000,
+				"memberSeq": 435,
+				"memberPhone": "18366155533",
+				"reservePhone": null,
+				"reserveShopTime": null,
+				"expressCompany": null,
+				"expressNo": null,
+				"deliveryTime": null,
+				"brandshopUserSeq": null,
+				"brandshopUserName": "张五",
+				"attrValueList": [
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 1,
+						"label": "家长姓名",
+						"name": "receiverName",
+						"value": "家长1"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "家长手机号",
+						"name": "detailAddress",
+						"value": "1388888888"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "宝宝姓名",
+						"name": "detailAddress",
+						"value": "宝宝1"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "宝宝身高",
+						"name": "detailAddress",
+						"value": "120"
+					},
+					{
+						"memberGiftAccountId": 457,
+						"attrId": 10,
+						"label": "宝宝证件号",
+						"name": "detailAddress",
+						"value": "32099999999999999"
+					},
+				], 
+			},
 		]
   }
   getHandleExpressGiftList() {
-    // let url = this.appConFig.API.;page=1
-	  // this.appService.httpGet(url).then( data => {
-    //   this.seflGiftArray = data;
-	  // }).catch(error => {
-    //  console.log(error);
-    // });
+    let url = `$(this.appConFig.API.)?brandshopSeq=$(this.brandshopSeqId)&type=3&start=$(this.start)&limit=10`;
+    this.appService.httpGet(url).then( data => {
+	    if (data.totalRecord == 0) {
+		    //空空如也
+		    this.noData = false;
+	    }else {
+		    this.noData = true;
+		    if( this.start < data.totalRecord ) {
+					if (this.up) {
+						this.handleExpressGiftArray.push(...data.data);
+		        		this.start+=10;
+					}else if (this.down){
+						this.handleExpressGiftArray = [...data.data];
+						this.start+=10;
+					}
+		     
+		    }else {
+		      this.showNoMoreGift = false;
+		    }
+	    }
+	
+	  }).catch(error => {
+		  console.log(error);
+	  });
+  }
+  refreshGetSelfGiftList(refresher) {
+	// 下拉刷新请求数据
+	// this.start = 0;
+	// this.down = true;
+	// this.up = false;
+	// setTimeout(() => {
+	//   this.getHandleExpressGiftList();
+	//   refresher.complete();
+	// },1000)
+  }
+  infiniteGetSelfGiftList(infiniteScroll) {
+	// 上拉刷新请求数据
+	// this.down = false;
+	// this.up = true;
+	// setTimeout(() => {
+	//   this.getHandleExpressGiftList();
+	//   infiniteScroll.complete();
+	// },1000)
   }
 }
--- src/pages/handle-selfgift/handle-selfgift.ts
+++ src/pages/handle-selfgift/handle-selfgift.ts
@@ -6,47 +6,138 @@ import { AppService, AppConfig } from '../../app/app.service';
   templateUrl: 'handle-selfgift.html'
 })
 export class HandleSelfgift {
-  seflGiftArray: any;
-	constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
-    //getHandleSelfGiftList()
+  handleSeflGiftArray: any;
+  noData: Boolean;
+  start: number;
+  showNoMoreGift: Boolean = true;
+  constructor(public navCtrl: NavController, 
+    public alertCtrl: AlertController, 
+    public appConFig: AppConfig, 
+    public appService: AppService,
+  ) {
+    // this.down = false;
+		// this.up = true;
+    //this.getHandleSelfGiftList()
 
     //模拟数据
-    this.seflGiftArray = [
+    this.handleSeflGiftArray = [
       {
-        id: 1,
-				name: "爱法呗赠品小马甲1",
-				memberPhone: "12131415161",
-				subscribePhone: "222222222",
-        getTime: "2017.09.27 23:59",
-        exchangeTime: "2017.09.27 11:59",
-				subscribeArriveTime: "2017.09.28 23:59",
-				imgUrl: "../assets/image/productimg.png",
-        subscribeState: "1",//1: 预约兑换，2：到店兑换
-        shoppingGuideName: "周三"
+        "memberGiftAccountSeq": 260,
+        "giftSeq": 11,
+        "giftCode": "COUPON00020154",
+        "giftName": "爱法呗赠品小马甲1",
+        "giftType": "0",
+        "imageName": "../assets/image/productimg.png",
+        "giftRemark": "1.适用年龄：0-2岁 2.课程总共4节 5.需提前预约，再到店兑换使用",
+        "brandshopSeq": 133,
+        "brandshopName": "1001夜杭州旗舰店",
+        "startDate": 1492706727000,
+        "endDate": 1493916327000,
+        "status": "0",
+        "receiveDate": 1492706727000,
+        "useDate": 1492713204000,
+        "memberSeq": 435,
+        "memberPhone": "18366155533",
+        "reservePhone": "13214567890",
+        "reserveShopTime": 1492713204000,
+        "expressCompany": null,
+        "expressNo": null,
+        "deliveryTime": null,
+        "brandshopUserSeq": 769,
+        "brandshopUserName": "周二",
+        "attrValueList": [],
       },
       {
-        id: 1,
-				name: "爱法呗赠品小马甲2",
-				memberPhone: "12131415161",
-				subscribePhone: "",
-        getTime: "2017.09.27 23:59",
-        exchangeTime: "2017.09.27 11:59",
-				subscribeArriveTime: "",
-				imgUrl: "../assets/image/productimg.png",
-        subscribeState: "2",//1: 预约兑换，2：到店兑换
-        shoppingGuideName: "王四"
-      }
+        "memberGiftAccountSeq": 260,
+        "giftSeq": 11,
+        "giftCode": "COUPON00020154",
+        "giftName": "爱法呗赠品小马甲2",
+        "giftType": "1",
+        "imageName": "../assets/image/productimg.png",
+        "giftRemark": "1.适用年龄：0-2岁 2.课程总共4节 5.需提前预约，再到店兑换使用",
+        "brandshopSeq": 133,
+        "brandshopName": "1001夜杭州旗舰店",
+        "startDate": 1492706727000,
+        "endDate": 1493916327000,
+        "status": "1",
+        "receiveDate": 1492706727000,
+        "useDate": 1492713204000,
+        "memberSeq": 435,
+        "memberPhone": "18366155533",
+        "reservePhone": "",
+        "reserveShopTime": null,
+        "expressCompany": null,
+        "expressNo": null,
+        "deliveryTime": null,
+        "brandshopUserSeq": 769,
+        "brandshopUserName": "周三",
+        "attrValueList": [],
+      },
+      // {
+      //   id: 1,
+			// 	name: "爱法呗赠品小马甲1",
+			// 	memberPhone: "12131415161",
+			// 	subscribePhone: "222222222",
+      //   getTime: "2017.09.27 23:59",
+      //   exchangeTime: "2017.09.27 11:59",
+			// 	subscribeArriveTime: "2017.09.28 23:59",
+			// 	imgUrl: "../assets/image/productimg.png",
+      //   subscribeState: "1",//1: 预约兑换，2：到店兑换
+      //   shoppingGuideName: "周三"
+      // },
+      // {
+      //   id: 1,
+			// 	name: "爱法呗赠品小马甲2",
+			// 	memberPhone: "12131415161",
+			// 	subscribePhone: "",
+      //   getTime: "2017.09.27 23:59",
+      //   exchangeTime: "2017.09.27 11:59",
+			// 	subscribeArriveTime: "",
+			// 	imgUrl: "../assets/image/productimg.png",
+      //   subscribeState: "2",//1: 预约兑换，2：到店兑换
+      //   shoppingGuideName: "王四"
+      // }
     ]
   }
   
   //进入页面，请求接口，得到数据
   getHandleSelfGiftList() {
-    // let url = this.appConFig.API.;page=1
-	  // this.appService.httpGet(url).then( data => {
-    //   this.seflGiftArray = data;
-	  // }).catch(error => {
-    //  console.log(error);
-    // });
+    let url = `$(this.appConFig.API.)?brandshopSeq=$(this.brandshopSeqId)&type=2&start=$(this.start)&limit=10`;
+    this.appService.httpGet(url).then( data => {
+    if (data.totalRecord == 0) {
+      //空空如也
+      this.noData = false;
+    }else {
+      this.noData = true;
+      if( this.start < data.totalRecord ) {
+        this.handleSeflGiftArray.push(...data.data);
+        this.start+=10;
+      }else {
+        this.showNoMoreGift = false;
+      }
+    }
+    
+    }).catch(error => {
+      console.log(error);
+    });
   }
-	
+	refreshGetSelfGiftList(refresher) {
+    // 下拉刷新请求数据
+    // this.start = 0;
+    // this.down = true;
+	  // this.up = false;
+    // setTimeout(() => {
+    //   this.getHandleSelfGiftList();
+    //   refresher.complete();
+    // },1000)
+  }
+  infiniteGetSelfGiftList(infiniteScroll) {
+    // 上拉刷新请求数据
+    // this.down = false;
+	  // this.up = true;
+    // setTimeout(() => {
+    //   this.getHandleSelfGiftList();
+    //   infiniteScroll.complete();
+    // },1000)
+  }
 }
