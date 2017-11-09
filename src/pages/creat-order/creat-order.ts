40bbfe91b380bc5ce0e7071631dd4e5ac98bf0ad
andyrew <bing.liu@61topbaby.com>
2017-11-09 21:35:23 +0800

    配单列表数据渲染

--- src/pages/creat-order/creat-order.ts
+++ src/pages/creat-order/creat-order.ts
@@ -2,22 +2,58 @@ import { Component} from '@angular/core';
 import { ModalController, NavController, NavParams, AlertController} from 'ionic-angular';
 import { OrderLayer } from '../order-layer/order-layer';
 import { OrderStore } from '../order-store/order-store';
+import { AppService, AppConfig } from '../../app/app.service';
 
 @Component({
   selector: 'creat-order',
   templateUrl: 'creat-order.html',
 })
 export class CreatOrder {
-  constructor(public modalCtrl: ModalController, public navCtrl: NavController, public alertCtrl: AlertController) {
+  productList: any;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  constructor(
+    public modalCtrl: ModalController, 
+    public navCtrl: NavController, 
+    public alertCtrl: AlertController,
+    public appService: AppService
+  ) {
+    this.productList = [{
+      id: '1',
+      imageName: "../assets/image/productimg2.png",
+      name: "1可爱小裙子 女童 秋冬 时尚QS1226603"
+    },{
+      id: '2',
+      imageName: "../assets/image/productimg2.png",
+      name: "2可爱小裙子 女童 秋冬 时尚QS1226603"
+    },{
+      id: '3',
+      imageName: "../assets/image/productimg2.png",
+      name: "3可爱小裙子 女童 秋冬 时尚QS1226603"
+    },{
+      id: '4',
+      imageName: "../assets/image/productimg2.png",
+      name: "4可爱小裙子 女童 秋冬 时尚QS1226603"
+    }];
   }
-  addProductModal() {
-	const orderModal = this.modalCtrl.create(OrderLayer, {id: 12}, {
-	cssClass: 'order-sku-list'
-	});
-	orderModal.present();
+  //获取商品列表
+  getProductList() {
+    // let url = `${AppConfig.API}&start=${(this.currentPage - 1) * this.pageSize}&limit=${this.pageSize}`;
+    // this.appService.httpGet(url).then(data => {
+    //   this.productList = data.body.productList;
+    //   this.currentPage ++;
+    // }).catch(error => {
+    //   console.log(error);
+    // });
+  }
+  addProductModal(item) {
+    const orderModal = this.modalCtrl.create(OrderLayer, {"product": item}, {
+      cssClass: 'order-sku-list'
+    });
+    orderModal.present();
   }
   orderRepertory () {
-	this.navCtrl.push(OrderStore);
+    this.navCtrl.push(OrderStore);
   }
 
 }
--- src/pages/order-layer/order-layer.ts
+++ src/pages/order-layer/order-layer.ts
@@ -1,5 +1,7 @@
 import { Component} from '@angular/core';
-import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { ModalController, NavController, NavParams, ViewController } from 'ionic-angular';
+import { OrderStore } from '../order-store/order-store';
+import { AppService, AppConfig } from '../../app/app.service';
 @Component({
   selector: 'order-layer',
   templateUrl: 'order-layer.html'
@@ -10,22 +12,49 @@ export class OrderLayer {
   SizeList: any;
   ColorList: any;
   count: number = 1;
-  constructor(public navCtrl: NavController, public viewCtrl: ViewController) {
-	this.SizeList = ["110", "120", "130", "140"];
-	this.ColorList = ["蓝色", "紫色", "黄色", "灰色"];
+  product: any;
+  skuList: any;
+  order: any;
+  constructor(
+    public modalCtrl: ModalController,
+    public navCtrl: NavController,
+    public viewCtrl: ViewController,
+    public navParams: NavParams,
+    public appService: AppService
+  ) {
+    // 获取当前产品
+    this.product = this.navParams.get("product");
+    console.log(this.product);
+    this.SizeList = ["110", "120", "130", "140"];
+    this.ColorList = ["蓝色", "紫色", "黄色", "灰色"];
+  }
+  // 获取商品sku属性
+  getSkuListById(id) {
+    // let url = `${AppConfig.API}&productId=${this.product.id}`;
+    // this.appService.httpGet(url)
+    //   .then(data => {
+    //     this.skuList = data.body.skuList;
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   }
+    // );
   }
   dismiss() {
-	let data = { 'foo': 'bar' };
-	this.viewCtrl.dismiss(data);
+    this.viewCtrl.dismiss();
+  }
+  goOrderStore() {
+    this.navCtrl.push(OrderStore, {"order": this.order});
+    this.viewCtrl.dismiss();
   }
   addCount() {
-	this.count++;
+    this.count++;
   }
   removeCount() {
-	this.count = this.count === 1 ? 1 : (this.count - 1);
+    this.count = this.count === 1 ? 1 : (this.count - 1);
   }
 //输入数字为负数时重置为1
   resetCount() {
-	this.count = this.count <= 0 ? 1 : this.count;
+    this.count = this.count <= 0 ? 1 : this.count;
   }
 }
