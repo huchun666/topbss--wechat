9e735b2587cdfa2640b08093389333ef34e775f6
andyrew <bing.liu@61topbaby.com>
2017-11-23 17:05:53 +0800

    审核中奖励活动,已审核奖励活动

--- src/pages/award-activity/award-activity.ts
+++ src/pages/award-activity/award-activity.ts
@@ -8,37 +8,56 @@ import { AppService, AppConfig } from '../../app/app.service';
 export class AwardActivity {
   pageSize: number = 10;
   currentPage: number = 1;
-  awardActivity: any = [{
-      "id": 1, //主键
-      "type": 1, //奖励类型：1-导购员处理订单；2-导购员发展会员；3-奖励活动导购员处理订单；4-奖励活动导购员发展会员；5-异业联盟门店发展会员
-      "relateId": 20151030001651,//关联ID:当type=1,3时，为orderId;当type=2,4,5时，为memberId
-      "userId": 1, //奖励对象ID:当type=1,2,3,4时，为brandshopUserId;当type=5时，为brandshopId(异业联盟门店ID);
-      "rewardActivityId": 1, //导购员奖励活动ID
-      "baseAmount": 699.00, //基础金额（基数）
-      "percent": 0.1000, //奖励比例
-      "amount": 69.90, //奖励金额
-      "returnAmount": 0.00, //退货扣除金额
-      "status": 1, //状态：0-异常锁定；1-正常（审核中）；2-已结算（可提现）
-      "startTime": 1481212800000, //奖励活动开始时间
-      "endTime": 1544284800000, //奖励活动结束时间
-      "createTime": 1509434651000, //创建时间
-      "updateTime": 1509434651000 //更新时间
-  }];
+  awardActivity: any = [];
+  count: number = 0;
+  sum: any;
+  isShow: boolean = false;
   constructor(
     public navCtrl: NavController, 
     public alertCtrl: AlertController,
     public appService: AppService
   ) {
-    // this.getOrderDetail();
+    this.getAwardActivity();
+    this.getBonusSum();
   }
-  getOrderDetail() {
-    let url = `${AppConfig.hostUrl + AppConfig.API.bonusList}?type=['1','3']&status=['0','1']$start=
-      ${(this.currentPage - 1) * this.pageSize}&limit=${this.pageSize}`;
+  getAwardActivity() {
+    let url = `${AppConfig.API.bonusList}?typeList=3,4&statusList=0,1&start=${(this.currentPage - 1) * this.pageSize}&limit=${this.pageSize}`;
     this.appService.httpGet(url)
       .then(data => {
-        this.awardActivity = data;
+        if (data.data.length > 0) {
+          data.data.map(item => {
+            item.baseAmount = item.baseAmount.toFixed(2);
+            item.percent = item.percent.toFixed(2);
+            item.amount = item.amount.toFixed(2);
+            item.returnAmount = item.returnAmount.toFixed(2);
+          });
+          this.awardActivity.push(...data.data);
+        }
+        this.count = data.count;
       }).catch(error => {
         console.log(error);
       });
+  }
+  // 获取总金额
+  getBonusSum() {
+    let url = `${AppConfig.API.bonusSum}?typeList=3,4&statusList=0,1`;
+    this.appService.httpGet(url)
+      .then(data => {
+        this.sum = data.sum;
+        this.setIsShow(this.sum);
+      }).catch(error => {
+        console.log(error);
+      });
+  }
+   // 有无明细列表时的判断（判断总金额是否为0）
+   setIsShow(sum) {
+    return this.isShow = sum > 0 ? true : false;
+  }
+  loadMore(infiniteScroll) {
+    this.currentPage ++;
+    setTimeout(() => {
+      this.getAwardActivity();
+      infiniteScroll.complete();
+    }, 500);
   }
 }
