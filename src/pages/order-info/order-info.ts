052f60adc773b0a244f536239409c2ab6e9e9a6c
andyrew <bing.liu@61topbaby.com>
2017-12-01 11:46:30 +0800

    代码格式调整

--- src/pages/order-info/order-info.ts
+++ src/pages/order-info/order-info.ts
@@ -7,41 +7,40 @@ import { AppService, AppConfig } from '../../app/app.service';
   templateUrl: 'order-info.html'
 })
 export class OrderInfo {
-	orderDetail: any;
+  orderDetail: any;
   constructor(
 		public navCtrl: NavController,
-    public alertCtrl: AlertController,
-		public viewCtrl: ViewController,
+		public alertCtrl: AlertController,
+    public viewCtrl: ViewController,
 		public navParams: NavParams,
-		public appService: AppService
+    public appService: AppService
   ) {
-
   }
   presentConfirm() {
-	  const alert = this.alertCtrl.create({
-		  message: '确认提货完成',
-		  buttons: [
-		    {
-		      text: '查看订单',  //type: 0
-		      handler: () => {
+    const alert = this.alertCtrl.create({
+			message: '确认提货完成',
+      buttons: [
+        {
+					text: '查看订单',  //type: 0
+          handler: () => {
 					  let data = { 'type': '0' };
-					  this.viewCtrl.dismiss(data);
-		      }
-		    },
-		    {
-		      text: '继续扫码',  //type: 1
-		      handler: () => {
-					  let data = { 'type': '1' };
-					  this.viewCtrl.dismiss(data);
-		        //this.barcodeScanner.scan().then((barcodeData) => {
-					  //  let myCodeModal = this.modalCtrl.create(OrderInfo, {});
-					  //  myCodeModal.present();
-					  //}, (err) => {
-					  //    console.log('扫码失败');
-					  //});
-		      }
-		    }
-		  ]
+						this.viewCtrl.dismiss(data);
+					}
+				},
+        {
+					text: '继续扫码',  //type: 1
+          handler: () => {
+						let data = { 'type': '1' };
+						this.viewCtrl.dismiss(data);
+            //this.barcodeScanner.scan().then((barcodeData) => {
+						//  let myCodeModal = this.modalCtrl.create(OrderInfo, {});
+						//  myCodeModal.present();
+            //}, (err) => {
+						//    console.log('扫码失败');
+						//});
+					}
+				}
+      ]
 	  });
     alert.present();
 	}
