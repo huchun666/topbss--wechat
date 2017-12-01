87198dab6b75d68df3f60690babad535a70812c5
王佳 <jia.wang@61topbaby.com>
2017-12-01 15:46:15 +0800

    代码格式调整

--- src/pages/handle-expressgift/handle-expressgift.ts
+++ src/pages/handle-expressgift/handle-expressgift.ts
@@ -13,112 +13,112 @@ export class HandleExpressgift {
   showNoMore: Boolean = false;
   noData: Boolean;
   up: Boolean;//上拉刷新和第一次进入页面时
-	down: Boolean;//下拉刷新和返回上一级页面时
-	load: any = {};
-	loadingShow: Boolean = true;
-	requestDefeat: Boolean = false;
+  down: Boolean;//下拉刷新和返回上一级页面时
+  load: any = {};
+  loadingShow: Boolean = true;
+  requestDefeat: Boolean = false;
   showInfinite: Boolean = false;
-	constructor(
-		public navCtrl: NavController, 
-		public alertCtrl: AlertController,
-		public appService: AppService,
-	) {
-	// 获取已兑换快递赠品列表
-		this.down = true;
-		this.up = false;
-		this.load = AppConfig.load;
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    public appService: AppService,
+  ) {
+    // 获取已兑换快递赠品列表
+    this.down = true;
+    this.up = false;
+    this.load = AppConfig.load;
     this.getHandleExpressGiftList()
   }
   getHandleExpressGiftList() {
-	let url = `${AppConfig.API.getGiftList}?brandshopSeq=133&type=1&start=${this.start}&limit=${this.limit}`;//brandshopSeq=${this.brandshopSeqId}
-	this.appService.httpGet(url).then( data => {
-		this.loadingShow = false;
-		if (data.count == 0) {
-			//空空如也
-			this.noData = true;
-		}else {
-			this.noData = false;
-			this.showInfinite = true;
-			if( this.start < data.count ) {
-				if (this.up) {
-					this.handleExpressGiftArray.push(...data.data);
-					this.start += this.limit;
-				}else if (this.down){
-					this.handleExpressGiftArray = data.data;
-					this.start += this.limit;
-					this.content.scrollTo(0,0,0); 
-				}
-			}else {
-				this.showNoMore = true;
-			}
-		}
-	}).catch(error => {
-    this.handleExpressGiftArray = [];
-		this.loadingShow = false;
-		console.log(error);
-		this.showInfinite = false;
-		this.requestDefeat = true;
-	});
+    let url = `${AppConfig.API.getGiftList}?brandshopSeq=133&type=1&start=${this.start}&limit=${this.limit}`;//brandshopSeq=${this.brandshopSeqId}
+    this.appService.httpGet(url).then(data => {
+      this.loadingShow = false;
+      if (data.count == 0) {
+        //空空如也
+        this.noData = true;
+      } else {
+        this.noData = false;
+        this.showInfinite = true;
+        if (this.start < data.count) {
+          if (this.up) {
+            this.handleExpressGiftArray.push(...data.data);
+            this.start += this.limit;
+          } else if (this.down) {
+            this.handleExpressGiftArray = data.data;
+            this.start += this.limit;
+            this.content.scrollTo(0, 0, 0);
+          }
+        } else {
+          this.showNoMore = true;
+        }
+      }
+    }).catch(error => {
+      this.handleExpressGiftArray = [];
+      this.loadingShow = false;
+      console.log(error);
+      this.showInfinite = false;
+      this.requestDefeat = true;
+    });
   }
 
   // 下拉刷新请求数据
   refreshGetHandleExpressGiftList(refresher) {
-	this.start = 0;
-	this.down = true;
-	this.up = false;
-	let url = `${AppConfig.API.getGiftList}?brandshopSeq=133&type=1&start=${this.start}&limit=${this.limit}`;
-    this.appService.httpGet(url).then( data => {
+    this.start = 0;
+    this.down = true;
+    this.up = false;
+    let url = `${AppConfig.API.getGiftList}?brandshopSeq=133&type=1&start=${this.start}&limit=${this.limit}`;
+    this.appService.httpGet(url).then(data => {
       refresher.complete();
       if (data.count == 0) {
         //空空如也
         this.noData = true;
-      }else {
-				this.noData = false;
-				this.showInfinite = true;
-				if (data.data.length != 0) {
-					this.handleExpressGiftArray = data.data;
-					this.start += this.limit;
-				}else {
-					this.showNoMore = true;
-				}
+      } else {
+        this.noData = false;
+        this.showInfinite = true;
+        if (data.data.length != 0) {
+          this.handleExpressGiftArray = data.data;
+          this.start += this.limit;
+        } else {
+          this.showNoMore = true;
+        }
       }
     }).catch(error => {
       this.handleExpressGiftArray = [];
       refresher.complete();
       console.log(error);
-			this.showInfinite = false;
+      this.showInfinite = false;
       this.requestDefeat = true;
     });
   }
 
   // 上拉刷新请求数据
   infiniteGetHandleExpressGiftList(infiniteScroll) {
-		this.down = false;
-		this.up = true;
-		let url = `${AppConfig.API.getGiftList}?brandshopSeq=133&type=1&start=${this.start}&limit=${this.limit}`;
-		this.appService.httpGet(url).then( data => {
-			infiniteScroll.complete();
-			if (data.count == 0) {
-				//空空如也
-				this.noData = true;
-			}else {
-				this.noData = false;
-				if (data.data.length != 0) {
-					this.handleExpressGiftArray.push(...data.data);
-					this.start += this.limit;
-				}else {
-					this.showNoMore = true;
-				}
-			}
-		}).catch(error => {
-			infiniteScroll.complete();
-			console.log(error);
-			this.appService.toast('网络异常，请稍后再试', 1000, 'middle');
-		});
-	}
-	
-	//请求失败后刷新
-	requestDefeatRefresh() {
+    this.down = false;
+    this.up = true;
+    let url = `${AppConfig.API.getGiftList}?brandshopSeq=133&type=1&start=${this.start}&limit=${this.limit}`;
+    this.appService.httpGet(url).then(data => {
+      infiniteScroll.complete();
+      if (data.count == 0) {
+        //空空如也
+        this.noData = true;
+      } else {
+        this.noData = false;
+        if (data.data.length != 0) {
+          this.handleExpressGiftArray.push(...data.data);
+          this.start += this.limit;
+        } else {
+          this.showNoMore = true;
+        }
+      }
+    }).catch(error => {
+      infiniteScroll.complete();
+      console.log(error);
+      this.appService.toast('网络异常，请稍后再试', 1000, 'middle');
+    });
+  }
+
+  //请求失败后刷新
+  requestDefeatRefresh() {
     this.requestDefeat = false;
     this.loadingShow = true;
     this.start = 0;
