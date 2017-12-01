a08c6dae926c2bc818640a72eb9a09037835ecea
andyrew <bing.liu@61topbaby.com>
2017-12-01 11:52:48 +0800

    代码空格tab格式调整

--- src/pages/order-info/order-info.ts
+++ src/pages/order-info/order-info.ts
@@ -9,48 +9,48 @@ import { AppService, AppConfig } from '../../app/app.service';
 export class OrderInfo {
   orderDetail: any;
   constructor(
-		public navCtrl: NavController,
-		public alertCtrl: AlertController,
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
     public viewCtrl: ViewController,
-		public navParams: NavParams,
+    public navParams: NavParams,
     public appService: AppService
   ) {
   }
   presentConfirm() {
     const alert = this.alertCtrl.create({
-			message: '确认提货完成',
+      message: '确认提货完成',
       buttons: [
         {
-					text: '查看订单',  //type: 0
+          text: '查看订单',  //type: 0
           handler: () => {
-					  let data = { 'type': '0' };
-						this.viewCtrl.dismiss(data);
-					}
-				},
+            let data = { 'type': '0' };
+            this.viewCtrl.dismiss(data);
+          }
+        },
         {
-					text: '继续扫码',  //type: 1
+          text: '继续扫码',  //type: 1
           handler: () => {
-						let data = { 'type': '1' };
-						this.viewCtrl.dismiss(data);
+          let data = { 'type': '1' };
+          this.viewCtrl.dismiss(data);
             //this.barcodeScanner.scan().then((barcodeData) => {
-						//  let myCodeModal = this.modalCtrl.create(OrderInfo, {});
-						//  myCodeModal.present();
+            //  let myCodeModal = this.modalCtrl.create(OrderInfo, {});
+            //  myCodeModal.present();
             //}, (err) => {
-						//    console.log('扫码失败');
-						//});
-					}
-				}
+            //    console.log('扫码失败');
+            //});
+          }
+        }
       ]
-	  });
+    });
     alert.present();
-	}
-	getOrderDetail() {
-		let url = this.navParams.get("url"); //提现总计，从当前账户传入过来;
+  }
+  getOrderDetail() {
+    let url = this.navParams.get("url"); //提现总计，从当前账户传入过来;
     this.appService.httpGet(url)
       .then(data => {
         this.orderDetail = data.data;
       }).catch(error => {
         console.log(error);
       });
-	}
-}
+    }
+  }
