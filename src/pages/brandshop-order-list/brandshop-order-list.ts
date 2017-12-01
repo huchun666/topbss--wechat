8b2dd0cb1f1c66705bbabb1bc225790c810a8a68
王佳 <jia.wang@61topbaby.com>
2017-12-01 09:45:17 +0800

    代码格式修正

--- src/pages/brandshop-order-list/brandshop-order-list.ts
+++ src/pages/brandshop-order-list/brandshop-order-list.ts
@@ -54,14 +54,11 @@ export class BrandshopOrderList {
         "d+": this.getDate(),       // day
       };
       if (/(y+)/.test(format)) {
-        format = format.replace(RegExp.$1, (this.getFullYear() + "")
-          .substr(4 - RegExp.$1.length));
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
       }
       for (var k in o) {
         if (new RegExp("(" + k + ")").test(format)) {
-          format = format.replace(RegExp.$1, RegExp.$1.length == 1
-            ? o[k]
-            : ("00" + o[k]).substr(("" + o[k]).length));
+          format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
         }
       }
       return format;
