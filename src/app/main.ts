a46f1f5eb9b6a6d400b092474de72b804bdf59c3
andyrew <bing.liu@61topbaby.com>
2018-01-03 13:40:17 +0800

    解决ios9下，时间显示不出来，并且报错cant't find variable: INTL

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,8 @@
+/** start 解决ios9下报错 “Can't find variable: Intl”， 时间显示不出来 **/
+import 'intl';
+import 'intl/locale-data/jsonp/en';
+/** end */
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
